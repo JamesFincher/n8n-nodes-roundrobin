d40f0bc7c7a0ac0c054db779c35ab46f63e78b43
James Fincher <heartandkarma@gmail.com>
2025-04-04 09:24:32 -0700

    v0.8.9: Fixed project structure and code quality

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'eslint-plugin-n8n-nodes-base'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js', 'dist/**/*', '*.json', '*.md'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
+    ],
+    'no-console': 'off',
+    'n8n-nodes-base/node-param-description-missing-from-dynamic-options': 'off',
+    'n8n-nodes-base/node-param-description-wrong-for-dynamic-options': 'off',
+  },
+}; 
--- n8n-nodes-roundrobin/dist/credentials/AirtableApi.credentials.d.ts
+++
@@ -1,8 +0,0 @@
-import { ICredentialType, INodeProperties } from 'n8n-workflow';
-export declare class AirtableApi implements ICredentialType {
-    name: string;
-    displayName: string;
-    documentationUrl: string;
-    properties: INodeProperties[];
-}
-//# sourceMappingURL=AirtableApi.credentials.d.ts.map
